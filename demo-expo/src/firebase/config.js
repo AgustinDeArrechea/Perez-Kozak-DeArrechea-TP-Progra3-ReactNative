90b85ffdf43bfa9cfdba2f09ca357766006edc33
AgustinDeArrechea <dearrecheaagustin@gmail.com>
2025-11-06 18:59:50 -0300

    conecte firebase

---
+++ demo-expo/src/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpIlQvgRlaY0nCEUBd6IjakVO6j693MjY",
+  authDomain: "reactnative-grupo9.firebaseapp.com",
+  projectId: "reactnative-grupo9",
+  storageBucket: "reactnative-grupo9.firebasestorage.app",
+  messagingSenderId: "999496409463",
+  appId: "1:999496409463:web:627afafb42d2dc35f3c172"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = app.firestore();
